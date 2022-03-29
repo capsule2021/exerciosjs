ee49a0c8f14fc02b164e42248600d1f4e840da44
capsule2021 <capsulebit@gmail.com>
2022-03-29 00:40:53 -0300

    estudos dados/desafioAreas/desafioTrocadeValores

---
+++ INICIO/dados_4.js
@@ -0,0 +1,9 @@
+const love = 4;
+
+
+//var love = 4;
+
+
+// love = love + 20;
+
+console.log(love);
---
+++ INICIO/desafio_area.js
@@ -0,0 +1,20 @@
+
+    //         Propriedades
+const PI = 3.141592;
+const raio =10;
+let areaCirc = PI * raio * raio;
+
+
+console.log("O valor de area e " + areaCirc + " m2."); 
+
+areaCirc = Math.PI * raio * raio;
+console.log(" O valor da area e " + areaCirc + "m2.");
+
+const E = 2.718;
+const quadrado =10;
+let quadradoCirc = E * quadrado * quadrado;
+
+console.log("O valor de area  e " + quadradoCirc + "m2.");
+
+quadradoCirc =Math.E  * quadrado * quadrado;
+console.log("O VALOR DE AREA E " + quadradoCirc + "m2.");
