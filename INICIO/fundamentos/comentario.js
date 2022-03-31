039d390ed6b99138abf13f678287e10b4b58729a
capsule2021 <capsulebit@gmail.com>
2022-03-31 17:38:14 -0300

    Fundamentos js

---
+++ INICIO/fundamentos/comentario.js
@@ -0,0 +1,16 @@
+// contomentario de uma linha
+console.log('Linha 1')
+
+/*
+comentario de multiplas linhas
+*/
+console.log('linha 2')
+
+
+/*
+* Comentario de
+* Multiplas linhas
+*
+* <stylo
+*/
+console.log('linha 3')
---
+++ INICIO/fundamentos/organizacao.js
@@ -0,0 +1,8 @@
+ console.log("Sentenca de codigo")
+
+ {
+     {
+         console.log("Ola mundo");
+         console.log("Mundo!!") // Padrao do curso
+     }
+ }
